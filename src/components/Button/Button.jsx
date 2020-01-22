e15ae974552e66e76a12414f7a89577b1b1dd070
S10-shota <s.o.0801105@gmail.com>
2020-01-23 00:01:45 +0900

    finish

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Button.css';
+
+const Button = (props) => {
+    let { onClickHandler } = props;
+
+    // onClickHandlerに関数以外の値がセットされていたら、
+    // ボタンクリック時にエラーを出さない為に何も中身のない
+    // 関数を実行する。
+    if (typeof onClickHandler !== 'function'){
+        onClickHandler = () => {};
+    }
+
+    return (
+        <div
+            className="Button"
+            onClick={onClickHandler}
+        >
+            {props.children}
+        </div>
+    );
+
+}
+
+export default Button;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+
+
+const Home = ({history}) => {
+    return(
+        <div>
+        <h1>HOME</h1>
+
+        <Button 
+            onClickHandler={() => {
+                history.push('/quiz');
+            }}
+        >
+            クイズページへ移動
+        </Button> 
+        </div>   
+    );
+
+};
+
+export default Home;
